f8b86d6c208f1eff4091165fe046aa82f4f5de5c
Vivek Baruah <vivekbrh01@gmail.com>
2020-10-21 22:58:38 +0530

    Interpolation solution

--- lib/bottles.rb
+++ lib/bottles.rb
@@ -13,17 +13,23 @@ class Bottles
 
 
   # Sparse Conditional Solution
-  def verse(number) 
-    if number == 99
-      n = 99 
-    else
-      n=3 
-    end
+  # def verse(number) 
+  #   if number == 99
+  #     n = 99 
+  #   else
+  #     n=3 
+  #   end
     
-    "#{n} bottles of beer on the wall, " +
-    "#{n} bottles of beer.\n" +
-    "Take one down and pass it around, " +
-    "#{n - 1} bottles of beer on the wall.\n"
+  #   "#{n} bottles of beer on the wall, " +
+  #   "#{n} bottles of beer.\n" +
+  #   "Take one down and pass it around, " +
+  #   "#{n - 1} bottles of beer on the wall.\n"
+  # end
+
+  # Interpolation Solution
+  def verse(number)
+    "#{number} bottles of beer on the wall, " + "#{number} bottles of beer.\n" +
+    "Take one down and pass it around, " + "#{number-1} bottles of beer on the wall.\n"
   end
 end
 

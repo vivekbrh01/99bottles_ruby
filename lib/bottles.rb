f995bdeed69924b779fccf8b109330982def09b6
Vivek Baruah <vivekbrh01@gmail.com>
2020-10-21 23:22:47 +0530

    Interpolated conditional solution for verse 2

--- lib/bottles.rb
+++ lib/bottles.rb
@@ -33,17 +33,25 @@ class Bottles
   # end
 
   # Stark Conditional Solution
+  # def verse(number)
+  #   if number == 2
+  #     "2 bottles of beer on the wall, " +
+  #     "2 bottles of beer.\n" +
+  #     "Take one down and pass it around, " + "1 bottle of beer on the wall.\n"
+  #   else
+  #     "#{number} bottles of beer on the wall, " + "#{number} bottles of beer.\n" +
+  #     "Take one down and pass it around, " + "#{number-1} bottles of beer on the wall.\n"
+  #   end
+  # end
+
+  # Interpolation Condiotional Solution
   def verse(number)
-    if number == 2
-      "2 bottles of beer on the wall, " +
-      "2 bottles of beer.\n" +
-      "Take one down and pass it around, " + "1 bottle of beer on the wall.\n"
-    else
-      "#{number} bottles of beer on the wall, " + "#{number} bottles of beer.\n" +
-      "Take one down and pass it around, " + "#{number-1} bottles of beer on the wall.\n"
-    end
-  end
-  
+    "#{number} bottles of beer on the wall, " + 
+    "#{number} bottles of beer.\n" +
+    "Take one down and pass it around, " + 
+    "#{number-1} bottle#{'s' unless (number-1) == 1} of beer " +
+    "on the wall.\n"
+  end  
 end
 
 # Incomprehensibly Concise
